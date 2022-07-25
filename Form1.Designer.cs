7fd701103f1d0f5fa4264373e9a9ea186f9eb0b7
Semih Sarı <semihsari152@hotmail.com>
2022-07-25 10:07:25 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace DatabaseManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtgw = new System.Windows.Forms.DataGridView();
+            this.lblQuery = new System.Windows.Forms.Label();
+            this.tbxQuery = new System.Windows.Forms.TextBox();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSelect = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgw)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtgw
+            // 
+            this.dtgw.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dtgw.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgw.Location = new System.Drawing.Point(12, 202);
+            this.dtgw.Name = "dtgw";
+            this.dtgw.ReadOnly = true;
+            this.dtgw.RowHeadersWidth = 62;
+            this.dtgw.RowTemplate.Height = 28;
+            this.dtgw.Size = new System.Drawing.Size(1212, 417);
+            this.dtgw.TabIndex = 0;
+            // 
+            // lblQuery
+            // 
+            this.lblQuery.AutoSize = true;
+            this.lblQuery.Font = new System.Drawing.Font("Arial Unicode MS", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblQuery.Location = new System.Drawing.Point(102, 43);
+            this.lblQuery.Name = "lblQuery";
+            this.lblQuery.Size = new System.Drawing.Size(84, 30);
+            this.lblQuery.TabIndex = 1;
+            this.lblQuery.Text = "Query :";
+            // 
+            // tbxQuery
+            // 
+            this.tbxQuery.Font = new System.Drawing.Font("Arial Unicode MS", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tbxQuery.Location = new System.Drawing.Point(192, 43);
+            this.tbxQuery.Name = "tbxQuery";
+            this.tbxQuery.Size = new System.Drawing.Size(740, 34);
+            this.tbxQuery.TabIndex = 2;
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Font = new System.Drawing.Font("Arial Unicode MS", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExecute.Location = new System.Drawing.Point(418, 127);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(206, 41);
+            this.btnExecute.TabIndex = 3;
+            this.btnExecute.Text = "Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Arial Unicode MS", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(1063, 32);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(147, 45);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Font = new System.Drawing.Font("Arial Unicode MS", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelect.Location = new System.Drawing.Point(1063, 123);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(147, 45);
+            this.btnSelect.TabIndex = 5;
+            this.btnSelect.Text = "All Columns";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1236, 639);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.tbxQuery);
+            this.Controls.Add(this.lblQuery);
+            this.Controls.Add(this.dtgw);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgw)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtgw;
+        private System.Windows.Forms.Label lblQuery;
+        private System.Windows.Forms.TextBox tbxQuery;
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSelect;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DatabaseManager
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;initial catalog=CarPrices;integrated security=true");
+
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+
+            string sql = tbxQuery.Text;
+            display(sql);
+        }
+        public void display(string sorgu)
+        {
+            try
+            {
+                var dt = new DataTable();
+                getCon();
+                var adp = new SqlDataAdapter($"{sorgu}", con);
+                adp.Fill(dt);
+                dtgw.DataSource = dt;
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public void getCon()
+        {
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            tbxQuery.Text = "";
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            tbxQuery.Text= "Select * from Cars";
+            string sql = "Select * from Cars";
+            display(sql);
+        }
+    }
+}
